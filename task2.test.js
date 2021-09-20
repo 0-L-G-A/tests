b17efed17e1727dbf3e01227fb951f0e9af2b861
0-L-G-A <lol79lol97@gmail.com>
2021-09-21 02:26:07 +0300

    tests

--- task2.test.js
+++ task2.test.js
@@ -1,13 +1,13 @@
 const friendNumbers = require("./task2");
 
 describe('friendNumbers', () => {
-    test(" should return array with subarrays with 3 numbers", () => {
+    test(" should return array", () => {
         expect(friendNumbers(200, 300)).not.toBeFalsy();
     });
-    test(' should return [2, 4, 6] if number is 56', () => {
+    test(' should return [284б 220] if number is 200, 300', () => {
         expect(friendNumbers(200, 300)).toEqual([284, 220]);
     })
-    test(' should throw an eror if the number is smaller than 3', () => {
+    test(' should throw an eror if the number is smaller than 200 or bigger than 300', () => {
         expect(() => friendNumbers(150, 300)).toThrow('Write correct numbers');
     })
 })
