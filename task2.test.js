caab9366a98d26dfcfb85bca55717f2d7e073156
0-L-G-A <lol79lol97@gmail.com>
2021-09-21 02:29:59 +0300

    tests

--- task2.test.js
+++ task2.test.js
@@ -4,10 +4,10 @@ describe('friendNumbers', () => {
     test(" should return array", () => {
         expect(friendNumbers(200, 300)).not.toBeFalsy();
     });
-    test(' should return [284б 220] if number is 200, 300', () => {
+    test(' should return [284, 220] if numbers are 200, 300', () => {
         expect(friendNumbers(200, 300)).toEqual([284, 220]);
     })
-    test(' should throw an eror if the number is smaller than 200 or bigger than 300', () => {
+    test(' should throw an eror if number is smaller than 200 or bigger than 300', () => {
         expect(() => friendNumbers(150, 300)).toThrow('Write correct numbers');
     })
 })
